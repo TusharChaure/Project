c840a3e150f3d723d0d2e806f8053e728b26b800
TusharChaure <chauretushar444@gmail.com>
2020-05-20 18:09:31 +0530

    Updates

---
+++ precache-manifest.98237d8a44bfb1ad0d79727cafb76b73.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efea7e8ea6aafa9d868762f0ebbdc550",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "19eb1f236b114c034770",
+    "url": "/home/static/css/2.a288efdd.chunk.css"
+  },
+  {
+    "revision": "5f3fd31fd5fcbb7c9dcf",
+    "url": "/home/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "19eb1f236b114c034770",
+    "url": "/home/static/js/2.f41faa6e.chunk.js"
+  },
+  {
+    "revision": "ec5a8f8a7f6b96dbf53d7fe7006559f7",
+    "url": "/home/static/js/2.f41faa6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f3fd31fd5fcbb7c9dcf",
+    "url": "/home/static/js/main.751362b0.chunk.js"
+  },
+  {
+    "revision": "32ad9becda3a0c4ee94e",
+    "url": "/home/static/js/runtime-main.b2ff94d9.js"
+  },
+  {
+    "revision": "7759920187286bc6d116f48d12460b09",
+    "url": "/home/static/media/Erfinden.77599201.png"
+  },
+  {
+    "revision": "c156626153c3d0711b6880ab4bbf13a8",
+    "url": "/home/static/media/background.c1566261.jpg"
+  },
+  {
+    "revision": "5ab06526db54f1396e6597693c7f4a69",
+    "url": "/home/static/media/bartar.5ab06526.png"
+  },
+  {
+    "revision": "f58495fa72e29655b1004d5a224a2770",
+    "url": "/home/static/media/chinese.f58495fa.jpg"
+  },
+  {
+    "revision": "82b8da413727c529a154d22161d92c54",
+    "url": "/home/static/media/delivery.82b8da41.PNG"
+  },
+  {
+    "revision": "11c0888c15fff4ca2efc5d02d0ea9fb5",
+    "url": "/home/static/media/drinks.11c0888c.jpg"
+  },
+  {
+    "revision": "535f2c9d79998735b1593132e5e65c85",
+    "url": "/home/static/media/first.535f2c9d.png"
+  },
+  {
+    "revision": "cccb4efc954c342d166998bc7d6a9314",
+    "url": "/home/static/media/girl.cccb4efc.jpg"
+  },
+  {
+    "revision": "3ae9282a48443ed03c00d2e7fc3660f7",
+    "url": "/home/static/media/glocal.3ae9282a.jpg"
+  },
+  {
+    "revision": "f694d61c3ee4e05404856ac0def3fded",
+    "url": "/home/static/media/home.f694d61c.png"
+  },
+  {
+    "revision": "f2fc139438a396250ee78a92b72e2bf3",
+    "url": "/home/static/media/indian.f2fc1394.jpeg"
+  },
+  {
+    "revision": "afc0f70e3a5323e94450228d684adabc",
+    "url": "/home/static/media/italian.afc0f70e.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/home/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "624e36bb8f31054ed1e786a3f4b25bd5",
+    "url": "/home/static/media/logo1.624e36bb.png"
+  },
+  {
+    "revision": "6bab57e0ace1e8673c4ed2bc4f14a639",
+    "url": "/home/static/media/meixcan.6bab57e0.jpeg"
+  },
+  {
+    "revision": "d6b97ae4e90c38a1cdb37000ca2c1816",
+    "url": "/home/static/media/off.d6b97ae4.PNG"
+  },
+  {
+    "revision": "88b9587ff6fa92fa9db5a6ed154a5c69",
+    "url": "/home/static/media/sec.88b9587f.jpg"
+  },
+  {
+    "revision": "1d83e4467ba9d3059b9922ea3820140f",
+    "url": "/home/static/media/subs.1d83e446.PNG"
+  },
+  {
+    "revision": "9fe292ebeb380bf82fdc55c951c3b662",
+    "url": "/home/static/media/tasty.9fe292eb.png"
+  }
+]);
